e5d8ac98e3e2ee809a3b4f899286ff8696654463
designer-ab <per.useravi0@gmail.com>
2024-07-03 21:08:53 +0600

    added navbar and footer with responsive

--- src/Layouts/Shared/Footer/Footer.jsx
+++ src/Layouts/Shared/Footer/Footer.jsx
@@ -1,9 +1,46 @@
+import logo from "../../../assets/logo/logo light.png";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
 const Footer = () => {
-    return (
-        <div>
-            <p>Footer</p>
-        </div>
-    );
+  return (
+    <div className="mt-10">
+      <footer className="footer bg-[#020043] text-base-content px-4 md:px-32 py-6 md:py-16">
+        <aside className="text-[#FFFFF5]">
+          <img src={logo} alt="" />
+          <p className="mt-4">
+            123 Main Street Anytown,USA <br /> Postal Code: 12345
+          </p>
+          <p className="mt-4">Support: support@oyolloo.com <br /> (Available :
+            10:00am to 07:00pm)</p>
+        </aside>
+        <nav className="align-baseline items-baseline text-[#FFFFF5] text-sm md:mt-12">
+          <a className="link link-hover">Home</a>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Success Page</a>
+          <a className="link link-hover">Terms and Condition</a>
+        </nav>
+        <nav className="align-baseline items-baseline text-[#FFFFF5] text-sm md:mt-12">
+          <a className="link link-hover">Services</a>
+          <a className="link link-hover">Scheduling</a>
+          <a className="link link-hover">Contact Us</a>
+          <a className="link link-hover">Patient Portal</a>
+        </nav>
+        <nav>
+          <a className="align-baseline items-baseline text-[#FFFFF5] text-sm md:mt-12">Follow us</a>
+          <div className="text-[#FFFFF5] flex gap-5 text-xl">
+            <p><FaFacebook /></p>
+            <p><FaInstagram /></p>
+            <p className="rounded-full"><FaLinkedin /></p>
+            <p><FaYoutube /></p>
+          </div>
+           <p className="text-[#FFFFF5] text-sm mt-6">@docplus 2024</p>
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/Layouts/Shared/Navbar/Navbar.jsx
+++ src/Layouts/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,74 @@
+import logo from "../../../assets/logo/logo dark.png";
+import { LuArrowUpRight } from "react-icons/lu";
+
 const Navbar = () => {
-    return (
-        <div>
-            <p>Navbar</p>
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-[#020043]"
+          >
+            <li>
+              <p>Home</p>
+            </li>
+
+            <li>
+              <p>Services</p>
+            </li>
+
+            <li>
+              <p>Blog</p>
+            </li>
+
+            <li>
+              <p>About us</p>
+            </li>
+          </ul>
         </div>
-    );
+        <img className="w-24" src={logo} alt="" />
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-[#020043]">
+          <li>
+            <p>Home</p>
+          </li>
+
+          <li>
+            <p>Services</p>
+          </li>
+
+          <li>
+            <p>Blog</p>
+          </li>
+
+          <li>
+            <p>About us</p>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="text-[#020043] font-medium border border-[#343268] rounded-xl px-6 py-2 flex items-center gap-1">Appointment <span className="text-xl"><LuArrowUpRight /></span> </a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
