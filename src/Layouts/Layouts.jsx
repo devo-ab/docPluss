6260871914f37d32686ef56481628b11e57d28ad
designer-ab <per.useravi0@gmail.com>
2024-07-04 10:32:56 +0600

    added font

--- src/Layouts/Layouts.jsx
+++ src/Layouts/Layouts.jsx
@@ -4,7 +4,7 @@ import Footer from "./Shared/Footer/Footer";
 
 const Layouts = () => {
   return (
-    <div>
+    <div className="font-inter">
       <div className="max-w-7xl mx-auto px-4 lg:px-0">
         <Navbar></Navbar>
         <Outlet></Outlet>
